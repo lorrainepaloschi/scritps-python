c63a7409b818e45ec572b7d6c5ec390112cb277a
Lorraine Vieira <lorraine.paloschi@gmail.com>
2022-02-22 16:17:52 -0300

    alteração para desafio 3, aula 04

--- aula 04/desafio-03.py
+++ aula 04/desafio-03.py
@@ -1,4 +1,4 @@
-n1 = input ('Insira o primeiro numero = ')
-n2 = input ('Insira o segundo numero = ')
-soma = int(n1)  + int(n2)
+n1 = int(input ('Insira o primeiro numero = '))
+n2 = int(input ('Insira o segundo numero = '))
+soma = n1+n2
 print('A soma é', soma)
