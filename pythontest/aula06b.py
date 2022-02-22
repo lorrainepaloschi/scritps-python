c16ecd6378778b984fbb242cc7b7bb140f3097f1
Lorraine Vieira <lorraine.paloschi@gmail.com>
2022-02-22 16:50:04 -0300

    verificação do tipo de conteúdo inserido

---
+++ pythontest/aula06b.py
@@ -0,0 +1,13 @@
+n = input('Digite algo: ')
+#print(type(n))
+#mostra o tipo
+
+#print(n.isnumeric())
+#mostra se é um numero ou não
+
+#print(n.isalpha())
+#mostra se são letras
+
+
+#print(n.isalnum())
+#mostra se é mistura de alphanumericos
