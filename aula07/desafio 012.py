851025154b4cf3740249245e9041e3b2994e5d38
Lorraine Vieira <lorraine.paloschi@gmail.com>
2022-02-23 17:21:57 -0300

    desafio 012

---
+++ aula07/desafio 012.py
@@ -0,0 +1,3 @@
+preco_antigo = float(input('Insira o valor original do produto: '))
+preco_com_desconto = preco_antigo - (preco_antigo * 0.05)
+print('O novo valor com 5% de desconto é {:.2f}R$' .format(preco_com_desconto))
