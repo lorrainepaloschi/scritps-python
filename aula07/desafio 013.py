4c00b27fcd8118a854cf836f16508f31cb594854
Lorraine Vieira <lorraine.paloschi@gmail.com>
2022-02-23 17:28:52 -0300

    desafio 013

---
+++ aula07/desafio 013.py
@@ -0,0 +1,3 @@
+salario_antigo = float(input('Informe o salário atual: '))
+salario_novo = salario_antigo + (salario_antigo * 0.15)
+print('O valor do salario com o novo aumento será de {}R$' .format(salario_novo))
