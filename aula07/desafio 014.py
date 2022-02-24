092052bce883084680ff2957e1a88ba501828c22
Lorraine Vieira <lorraine.paloschi@gmail.com>
2022-02-24 11:38:01 -0300

    alteração desafio 014

---
+++ aula07/desafio 014.py
@@ -0,0 +1,4 @@
+celsius = float(input('Informe a temperatura em ºC: '))
+f = (celsius * 1.8) + 32
+# f = ((9 * celsius) / 5) + 32
+print('A temperatura de {}ºC corresponde a {}ºF' .format(celsius, f))
