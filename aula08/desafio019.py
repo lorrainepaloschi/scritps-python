febaeae4faeb6a0f4d92a8df5570b6553da80ff1
Lorraine Vieira <lorraine.paloschi@gmail.com>
2022-03-02 23:27:56 -0300

    desafio 019

---
+++ aula08/desafio019.py
@@ -0,0 +1,11 @@
+import random
+
+aluno1 = input('Insira o nome do 1º aluno: ')
+aluno2 = input('Insira o nome do 2º aluno: ')
+aluno3 = input('Insira o nome do 3º aluno: ')
+aluno4 = input('Insira o nome do 4º aluno: ')
+
+list = (aluno1, aluno2, aluno3, aluno4)
+escolhido = random.choice(list)
+print('o nome sorteado foi: {}' .format(escolhido))
+
